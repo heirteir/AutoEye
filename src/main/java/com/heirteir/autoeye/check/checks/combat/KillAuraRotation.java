824a369c0980a798301879c90aa096a754f523af
BuildTools <unconfigured@null.spigotmc.org>
2018-04-11 07:11:48 -0400

    Added timing to KillAuraRotation check. For some reason it didn't add this the first time.

--- src/main/java/com/heirteir/autoeye/check/checks/combat/KillAuraRotation.java
+++ src/main/java/com/heirteir/autoeye/check/checks/combat/KillAuraRotation.java
@@ -5,30 +5,18 @@ import com.heirteir.autoeye.check.Check;
 import com.heirteir.autoeye.event.events.PacketPlayInFlyingEvent;
 
 public class KillAuraRotation extends Check<PacketPlayInFlyingEvent> {
-
-    /*
-    * Created by Leo101 ~11/4/18
-    *
-    * Dont forget to commit :3
-     */
-
     public KillAuraRotation() {
         super("KillAura (Rotation)");
     }
 
     @Override public boolean check(Autoeye autoeye, PacketPlayInFlyingEvent event) {
-        if (Math.abs(event.getPacket().getYaw() - event.getPlayer().getPhysics().getPreviousPacketPlayInFlying().getYaw()) > 45.0F
-                && Math.abs(event.getPacket().getPitch() - event.getPlayer().getPhysics().getPreviousPacketPlayInFlying().getPitch()) < 2.0F) {
-            return this.checkThreshold(event.getPlayer(), 5, 5000L);
-        }
-        return false;
+        return Math.abs(event.getPacket().getYaw() - event.getPlayer().getPhysics().getPreviousPacketPlayInFlying().getYaw()) > 45.0F && Math.abs(event.getPacket().getPitch() - event.getPlayer().getPhysics().getPreviousPacketPlayInFlying().getPitch()) < 2.0F && this.checkThreshold(event.getPlayer(), 5, 5000L);
     }
 
     @Override public boolean canRun(PacketPlayInFlyingEvent event) {
-        // if System.currentTimeMills() - lastUseEntity < 300L
-        return false;
+        return event.getPlayer().getTimeData().getLastUseEntity().getDifference() < 300L;
     }
 
-    @Override
-    public void revert(Autoeye autoeye, PacketPlayInFlyingEvent event) {}
+    @Override public void revert(Autoeye autoeye, PacketPlayInFlyingEvent event) {
+    }
 }
