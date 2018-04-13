63fc0755588d9e61cbea45c9409d4454ed270499
BuildTools <unconfigured@null.spigotmc.org>
2018-04-13 02:17:03 -0400

    Removed velocity from the autoeye command, and improved Invalid Motion check.

--- src/main/java/com/heirteir/autoeye/check/checks/movement/InvalidMotion.java
+++ src/main/java/com/heirteir/autoeye/check/checks/movement/InvalidMotion.java
@@ -3,7 +3,6 @@ package com.heirteir.autoeye.check.checks.movement;
 import com.heirteir.autoeye.Autoeye;
 import com.heirteir.autoeye.check.Check;
 import com.heirteir.autoeye.event.events.event.PacketPlayInFlyingEvent;
-import com.heirteir.autoeye.player.data.Physics;
 
 public class InvalidMotion extends Check<PacketPlayInFlyingEvent> {
     public InvalidMotion() {
@@ -12,17 +11,9 @@ public class InvalidMotion extends Check<PacketPlayInFlyingEvent> {
 
     @Override public boolean check(Autoeye autoeye, PacketPlayInFlyingEvent event) {
         if (event.getPlayer().getPhysics().getClientVelocity().getY() > event.getPlayer().getPhysics().getCalculatedYVelocity() + 0.001F && Math.abs(event.getPlayer().getPhysics().getClientAcceleration().getY() - event.getPlayer().getPhysics().getCalculatedYAcceleration()) > 0.002) {
-            System.out.println(event.getPlayer().getPhysics().getCalculatedYVelocity() + " " + event.getPlayer().getPhysics().getClientVelocity().getY());
-            if (event.getPlayer().getPhysics().getClientVelocity().getY() == 0 && event.getPlayer().getTimeData().getLastTeleport().getDifference() < 1000) {
-                return this.checkThreshold(event.getPlayer(), 5, 500L);
-            }
-            return !((event.getPlayer().getPhysics().getClientVelocity().getY() == 0) || (event.getPlayer().getPhysics().getCalculatedYVelocity() > 0 && event.getPlayer().getPhysics().getCalculatedYVelocity() < 0.04)) || this.checkThreshold(event.getPlayer(), 2, 100L);
+            return (event.getPlayer().getPhysics().getClientVelocity().getY() == 0 && event.getPlayer().getTimeData().getLastTeleport().getDifference() < 1000 && this.checkThreshold(event.getPlayer(), 5, 500L)) || !((event.getPlayer().getPhysics().getClientVelocity().getY() == 0) || (event.getPlayer().getPhysics().getCalculatedYVelocity() > 0 && event.getPlayer().getPhysics().getCalculatedYVelocity() < 0.04)) || this.checkThreshold(event.getPlayer(), 2, 100L);
         } else {
-            if (Math.abs(event.getPlayer().getPhysics().getCalculatedYVelocity() - event.getPlayer().getPlayer().getVelocity().getY()) < 0.1 && Math.abs(event.getPlayer().getPhysics().getClientVelocity().getY() - event.getPlayer().getPhysics().getCalculatedYVelocity()) > 0.001 && Math.abs(event.getPlayer().getPhysics().getClientAcceleration().getY() - event.getPlayer().getPhysics().getCalculatedYAcceleration()) > 0.002 && (!(event.getPlayer().getPhysics().getClientVelocity().getY() == -0.07839966F) || this.checkThreshold(event.getPlayer(), 2, 100L))) {
-                System.out.println(event.getPlayer().getPhysics().getCalculatedYVelocity() + " " + event.getPlayer().getPhysics().getClientVelocity().getY());
-                return true;
-            }
-            return false;
+            return Math.abs(event.getPlayer().getPhysics().getCalculatedYVelocity() - event.getPlayer().getPlayer().getVelocity().getY()) < 0.1 && Math.abs(event.getPlayer().getPhysics().getClientVelocity().getY() - event.getPlayer().getPhysics().getCalculatedYVelocity()) > 0.001 && Math.abs(event.getPlayer().getPhysics().getClientAcceleration().getY() - event.getPlayer().getPhysics().getCalculatedYAcceleration()) > 0.002 && (!(event.getPlayer().getPhysics().getClientVelocity().getY() == -0.07839966F) || this.checkThreshold(event.getPlayer(), 2, 100L));
         }
     }
 
